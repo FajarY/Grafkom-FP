4aadfcd21ecc5845a534cda31573266ffd467ea0
yaudahbanh <juniorvincent32@gmail.com>
2025-11-30 21:13:15 +0700

    feat: add wajan and daging also daging texture

--- src/script.ts
+++ src/script.ts
@@ -277,6 +277,13 @@ async function setupKitchen()
     table.position.set(0.0, 0.5, 0.0);
     scene.add(table);
 
+    // const daging = await loadFBX("daging.fbx");
+    // daging.scale.setScalar(0.002);
+    // daging.position.set(-0.20, 1.05, -0.20);
+    // // daging.rotateX(MathUtils.degToRad(90.0));
+    // scene.add(daging);
+    // makeDraggable(daging);
+
     const knife = await loadGLTF("source/knife2.glb");
     knife.scale.setScalar(0.25);
     knife.position.set(0.20, 1.05, -0.20);
